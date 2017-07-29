45c17bbbe243be98038ed639bd64605935d7d543
Dzejrou <dzejrou@gmail.com>
2017-07-29 18:51:44 +0200

    Started working on the Mos6507 cpu.

---
+++ src/cpus/mos6507.rs
@@ -0,0 +1,71 @@
+use cpus::Cpu;
+use mems::Memory;
+use regs::Registers;
+
+enum AddressMode {
+    Immediate,
+    ZeroPage,
+    ZeroPageX,
+    Absolute,
+    AbsoluteX,
+    AbsoluteY,
+    IndirectX,
+    IndirectY,
+    Relative,
+    Accumulator,
+    None
+}
+
+pub struct Mos6507<M: Memory, R: Registers> {
+    ram: M,
+    regs: R,
+    pc: u16,
+    addr_mode: AddressMode,
+}
+
+impl<M: Memory, R: Registers> Cpu<M, R> for Mos6507<M, R> {
+    fn memory(&self) -> &M {
+        &self.ram
+    }
+
+    fn registers(&self) -> &R {
+        &self.regs
+    }
+
+    fn run(&mut self, rom : &Vec<u8>) {
+        // TODO: PC initial value.
+        loop {
+            self.execute(rom);
+            self.pc += self.pc_offset();
+        }
+    }
+}
+
+impl<M: Memory, R: Registers> Mos6507<M, R> {
+    pub fn new(ram: M, regs: R) -> Mos6507<M, R> {
+        Mos6507 {
+            ram,
+            regs,
+            pc: 0u16,
+            addr_mode: AddressMode::None
+        }
+    }
+
+    fn execute(&mut self, rom: &Vec<u8>) {
+    
+    }
+
+    fn pc_offset(&self) -> u16 {
+        match self.addr_mode {
+            AddressMode::Absolute  |
+            Addressmode::AbsoluteX |
+            AddressMode::AbsoluteY => 3,
+            AddressMode::None      => 1,
+            _                      => 2,
+        }
+    }
+
+    fn get_addr_mode(&self, opcode: u8) {
+        0
+    }
+}
