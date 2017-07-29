e55d85cb549e8898ba4dc558ce3bbcdbf564bf22
Dzejrou <dzejrou@gmail.com>
2017-07-30 01:30:24 +0200

    Added opcode handling execution, empty bodies for opcode handlers, some opcode bodies, etc.

--- src/cpus/mos6507.rs
+++ src/cpus/mos6507.rs
@@ -282,7 +282,21 @@ enum AddressMode {
 pub struct Mos6507<M: Memory> {
     ram: M,
     pc: u16,
+    sp: u8,
+    idx_x: u8,
+    idx_y: u8,
+    accu: u8,
     addr_mode: AddressMode,
+
+    // These are supposed to be flags,
+    // but we have the memory.
+    carry: bool,
+    break_cmd: bool,
+    decimal: bool,
+    interrupt_disable: bool,
+    negative: bool,
+    overflow: bool,
+    zero: bool
 }
 
 impl<M: Memory> Cpu<M> for Mos6507<M> {
@@ -304,12 +318,241 @@ impl<M: Memory> Mos6507<M> {
         Mos6507 {
             ram,
             pc: 0u16,
-            addr_mode: AddressMode::None
+            sp: 0u8,
+            idx_x: 0u8,
+            idx_y: 0u8,
+            accu: 0u8,
+            addr_mode: AddressMode::None,
+            carry: false,
+            break_cmd: false,
+            decimal: false,
+            interrupt_disable: false,
+            negative: false,
+            overflow: false,
+            zero: false
         }
     }
 
     fn execute(&mut self, cart: &Memory) {
         // TODO: Get opcode, set addrmode, execute.
+        let opcode = cart.read_u8(self.pc as usize);
+        self.addr_mode = self.get_addr_mode(opcode);
+        let operand = self.get_operand();
+
+        match opcode {
+            OP_ADC_IMMEDIATE   |
+            OP_ADC_ZERO_PAGE   |
+            OP_ADC_ZERO_PAGE_X |
+            OP_ADC_ABSOLUTE    |
+            OP_ADC_ABSOLUTE_X  |
+            OP_ADC_ABSOLUTE_Y  |
+            OP_ADC_INDIRECT_X  |
+            OP_ADC_INDIRECT_Y  => self.op_adc(operand),
+
+            OP_AND_IMMEDIATE   |
+            OP_AND_ZERO_PAGE   |
+            OP_AND_ZERO_PAGE_X |
+            OP_AND_ABSOLUTE    |
+            OP_AND_ABSOLUTE_X  |
+            OP_AND_ABSOLUTE_Y  |
+            OP_AND_INDIRECT_X  |
+            OP_AND_INDIRECT_Y  => self.op_and(operand),
+
+            OP_ASL_ACCUMULATOR |
+            OP_ASL_ZERO_PAGE   |
+            OP_ASL_ZERO_PAGE_X |
+            OP_ASL_ABSOLUTE    |
+            OP_ASL_ABSOLUTE_X  => self.op_asl(operand),
+
+            OP_BCC_RELATIVE    => self.op_bcc(operand),
+
+            OP_BCS_RELATIVE    => self.op_bcs(operand),
+
+            OP_BEQ_RELATIVE    => self.op_beq(operand),
+
+            OP_BIT_ZERO_PAGE   |
+            OP_BIT_ABSOLUTE    => self.op_bit(operand),
+
+            OP_BMI_RELATIVE    => self.op_bmi(operand),
+
+            OP_BNE_RELATIVE    => self.op_bne(operand),
+
+            OP_BPL_RELATIVE    => self.op_bpl(operand),
+
+            OP_BRK_IMPLIED     => self.op_brk(operand),
+
+            OP_BVC_RELATIVE    => self.op_bvc(operand),
+
+            OP_BVS_RELATIVE    => self.op_bvs(operand),
+
+            OP_CLC_IMPLIED     => self.op_clc(),
+
+            OP_CLD_IMPLIED     => self.op_cld(),
+
+            OP_CLI_IMPLIED     => self.op_cli(),
+
+            OP_CLV_IMPLIED     => self.op_clv(),
+
+            OP_CMP_IMMEDIATE   |
+            OP_CMP_ZERO_PAGE   |
+            OP_CMP_ZERO_PAGE_X |
+            OP_CMP_ABSOLUTE    |
+            OP_CMP_ABSOLUTE_X  |
+            OP_CMP_ABSOLUTE_Y  |
+            OP_CMP_INDIRECT_X  |
+            OP_CMP_INDIRECT_Y  => self.op_cmp(operand),
+
+            OP_CPX_IMMEDIATE   |
+            OP_CPX_ZERO_PAGE   |
+            OP_CPX_ABSOLUTE    => self.op_cpx(operand),
+
+            OP_CPY_IMMEDIATE   |
+            OP_CPY_ZERO_PAGE   |
+            OP_CPY_ABSOLUTE    => self.op_cpy(operand),
+
+            OP_DEC_ZERO_PAGE   |
+            OP_DEC_ZERO_PAGE_X |
+            OP_DEC_ABSOLUTE    |
+            OP_DEC_ABSOLUTE_X  => self.op_dec(operand),
+
+            OP_DEX_IMPLIED     => self.op_dex(operand),
+
+            OP_DEY_IMPLIED     => self.op_dey(operand),
+
+            OP_EOR_IMMEDIATE   |
+            OP_EOR_ZERO_PAGE   |
+            OP_EOR_ZERO_PAGE_X |
+            OP_EOR_ABSOLUTE    |
+            OP_EOR_ABSOLUTE_X  |
+            OP_EOR_ABSOLUTE_Y  |
+            OP_EOR_INDIRECT_X  |
+            OP_EOR_INDIRECT_Y  => self.op_eor(operand),
+
+            OP_INC_ZERO_PAGE   |
+            OP_INC_ZERO_PAGE_X |
+            OP_INC_ABSOLUTE    |
+            OP_INC_ABSOLUTE_X  => self.op_inc(operand),
+
+            OP_INX_IMPLIED     => self.op_inx(operand),
+
+            OP_INY_IMPLIED     => self.op_iny(operand),
+
+            OP_JMP_ABSOLUTE    |
+            OP_JMP_INDIRECT    => self.op_jmp(operand),
+
+            OP_JSR_ABSOLUTE    => self.op_jsr(operand),
+
+            OP_LDA_IMMEDIATE   |
+            OP_LDA_ZERO_PAGE   |
+            OP_LDA_ZERO_PAGE_X |
+            OP_LDA_ABSOLUTE    |
+            OP_LDA_ABSOLUTE_X  |
+            OP_LDA_ABSOLUTE_Y  |
+            OP_LDA_INDIRECT_X  |
+            OP_LDA_INDIRECT_Y  => self.op_lda(operand),
+
+            OP_LDX_IMMEDIATE   |
+            OP_LDX_ZERO_PAGE   |
+            OP_LDX_ZERO_PAGE_Y |
+            OP_LDX_ABSOLUTE    |
+            OP_LDX_ABSOLUTE_Y  => self.op_ldx(operand),
+
+            OP_LDY_IMMEDIATE   |
+            OP_LDY_ZERO_PAGE   |
+            OP_LDY_ZERO_PAGE_X |
+            OP_LDY_ABSOLUTE    |
+            OP_LDY_ABSOLUTE_X  => self.op_ldy(operand),
+
+            OP_LSR_ACCUMULATOR |
+            OP_LSR_ZERO_PAGE   |
+            OP_LSR_ZERO_PAGE_X |
+            OP_LSR_ABSOLUTE    |
+            OP_LSR_ABSOLUTE_X  => self.op_lsr(operand),
+
+            OP_NOP_IMPLIED     => self.op_nop(),
+
+            OP_ORA_IMMEDIATE   |
+            OP_ORA_ZERO_PAGE   |
+            OP_ORA_ZERO_PAGE_X |
+            OP_ORA_ABSOLUTE    |
+            OP_ORA_ABSOLUTE_X  |
+            OP_ORA_ABSOLUTE_Y  |
+            OP_ORA_INDIRECT_X  |
+            OP_ORA_INDIRECT_Y  => self.op_ora(operand),
+
+            OP_PHA_IMPLIED     => self.op_pha(operand),
+
+            OP_PHP_IMPLIED     => self.op_php(operand),
+
+            OP_PLA_IMPLIED     => self.op_pla(operand),
+
+            OP_PLP_IMPLIED     => self.op_plp(operand),
+
+            OP_ROL_ACCUMULATOR |
+            OP_ROL_ZERO_PAGE   |
+            OP_ROL_ZERO_PAGE_X |
+            OP_ROL_ABSOLUTE    |
+            OP_ROL_ABSOLUTE_X  => self.op_rol(operand),
+
+            OP_ROR_ACCUMULATOR |
+            OP_ROR_ZERO_PAGE   |
+            OP_ROR_ZERO_PAGE_X |
+            OP_ROR_ABSOLUTE    |
+            OP_ROR_ABSOLUTE_X  => self.op_ror(operand),
+
+            OP_RTI_IMPLIED     => self.op_rti(operand),
+
+            OP_RTS_IMPLIED     => self.op_rts(operand),
+
+            OP_SBC_IMMEDIATE   |
+            OP_SBC_ZERO_PAGE   |
+            OP_SBC_ZERO_PAGE_X |
+            OP_SBC_ABSOLUTE    |
+            OP_SBC_ABSOLUTE_X  |
+            OP_SBC_ABSOLUTE_Y  |
+            OP_SBC_INDIRECT_X  |
+            OP_SBC_INDIRECT_Y  => self.op_sbc(operand),
+
+            OP_SEC_IMPLIED     => self.op_sec(),
+
+            OP_SED_IMPLIED     => self.op_sed(),
+
+            OP_SEI_IMPLIED     => self.op_sei(),
+
+            OP_STA_ZERO_PAGE   |
+            OP_STA_ZERO_PAGE_X |
+            OP_STA_ABSOLUTE    |
+            OP_STA_ABSOLUTE_X  |
+            OP_STA_ABSOLUTE_Y  |
+            OP_STA_INDIRECT_X  |
+            OP_STA_INDIRECT_Y  => self.op_sta(operand),
+
+            OP_STX_ZERO_PAGE   |
+            OP_STX_ZERO_PAGE_Y |
+            OP_STX_ABSOLUTE    => self.op_stx(operand),
+
+            OP_STY_ZERO_PAGE   |
+            OP_STY_ZERO_PAGE_X |
+            OP_STY_ABSOLUTE    => self.op_sty(operand),
+
+            OP_TAX_IMPLIED     => self.op_tax(operand),
+
+            OP_TAY_IMPLIED     => self.op_tay(operand),
+
+            OP_TYA_IMPLIED     => self.op_tya(operand),
+
+            OP_TSX_IMPLIED     => self.op_tsx(operand),
+
+            OP_TXA_IMPLIED     => self.op_txa(operand),
+
+            OP_TXS_IMPLIED     => self.op_txs(operand),
+
+            op => panic!("Unknown opcode: {}", op)
+        }
+    }
+
+    fn get_operand(&self) -> u8 {
+        0
     }
 
     fn pc_offset(&self) -> u16 {
@@ -461,5 +704,247 @@ impl<M: Memory> Mos6507<M> {
 
             _ => AddressMode::None,
         }
+    }
+
+    fn op_adc(&mut self, operand: u8) {
+        if self.decimal {
+            panic!("Decimal addition not implemented yet!");
+        } else {
+            let mut result = self.accu.wrapping_add(operand);
+            result = result.wrapping_add(self.carry as u8);
+
+            let mut check = (self.accu as u16) + (self.carry as u16);
+            check +=  operand as u16;
+
+            self.carry = check > 255;
+
+            let signed = result as i16;
+            self.overflow = signed > 127 || signed < -128;
+        }
+
+        let result = self.accu;
+        self.zero = result == 0;
+        self.negative = (result as i8) < 0;
+    }
+
+    fn op_and(&mut self, operand: u8) {
+    
+    }
+
+    fn op_asl(&mut self, operand: u8) {
+    
+    }
+
+    fn op_bcc(&mut self, operand: u8) {
+    
+    }
+
+    fn op_bcs(&mut self, operand: u8) {
+    
+    }
+
+    fn op_beq(&mut self, operand: u8) {
+    
+    }
+
+    fn op_bit(&mut self, operand: u8) {
+    
+    }
+
+    fn op_bmi(&mut self, operand: u8) {
+    
+    }
+
+    fn op_bne(&mut self, operand: u8) {
+    
+    }
+
+    fn op_bpl(&mut self, operand: u8) {
+    
+    }
+
+    fn op_brk(&mut self, operand: u8) {
+        self.break_cmd = true;
+    }
+
+    fn op_bvc(&mut self, operand: u8) {
+    
+    }
+
+    fn op_bvs(&mut self, operand: u8) {
+    
+    }
+
+    fn op_clc(&mut self) {
+        self.carry = true;
+    }
+
+    fn op_cld(&mut self) {
+        self.decimal = true;
+    }
+
+    fn op_cli(&mut self) {
+        self.interrupt_disable = false;
+    }
+
+    fn op_clv(&mut self) {
+        self.overflow = false;
+    }
+
+    fn op_cmp(&mut self, operand: u8) {
+    
+    }
+
+    fn op_cpx(&mut self, operand: u8) {
+    
+    }
+
+    fn op_cpy(&mut self, operand: u8) {
+    
+    }
+
+    fn op_dec(&mut self, operand: u8) {
+    
+    }
+
+    fn op_dex(&mut self, operand: u8) {
+    
+    }
+
+    fn op_dey(&mut self, operand: u8) {
+    
+    }
+
+    fn op_eor(&mut self, operand: u8) {
+    
+    }
+
+    fn op_inc(&mut self, operand: u8) {
+    
+    }
+
+    fn op_inx(&mut self, operand: u8) {
+    
+    }
+
+    fn op_iny(&mut self, operand: u8) {
+    
+    }
+
+    fn op_jmp(&mut self, operand: u8) {
+    
+    }
+
+    fn op_jsr(&mut self, operand: u8) {
+    
+    }
+
+    fn op_lda(&mut self, operand: u8) {
+    
+    }
+
+    fn op_ldx(&mut self, operand: u8) {
+    
+    }
+
+    fn op_ldy(&mut self, operand: u8) {
+    
+    }
+
+    fn op_lsr(&mut self, operand: u8) {
+    
+    }
+
+    fn op_nop(&mut self) {
+        // Nothing done.
+    }
+
+    fn op_ora(&mut self, operand: u8) {
+    
+    }
+
+    fn op_pha(&mut self, operand: u8) {
+    
+    }
+
+    fn op_php(&mut self, operand: u8) {
+    
+    }
+
+    fn op_pla(&mut self, operand: u8) {
+    
+    }
+
+    fn op_plp(&mut self, operand: u8) {
+    
+    }
+
+    fn op_rol(&mut self, operand: u8) {
+    
+    }
+
+    fn op_ror(&mut self, operand: u8) {
+    
+    }
+
+    fn op_rti(&mut self, operand: u8) {
+    
+    }
+
+    fn op_rts(&mut self, operand: u8) {
+    
+    }
+
+    fn op_sbc(&mut self, operand: u8) {
+    
+    }
+
+    fn op_sec(&mut self) {
+        self.carry = true;
+    }
+
+    fn op_sed(&mut self) {
+        self.decimal = true;
+    
+    }
+
+    fn op_sei(&mut self) {
+        self.interrupt_disable = true;
+    }
+
+    fn op_sta(&mut self, operand: u8) {
+    
+    }
+
+    fn op_stx(&mut self, operand: u8) {
+    
+    }
+
+    fn op_sty(&mut self, operand: u8) {
+    
+    }
+
+    fn op_tax(&mut self, operand: u8) {
+    
+    }
+
+    fn op_tay(&mut self, operand: u8) {
+    
+    }
+
+    fn op_tya(&mut self, operand: u8) {
+    
+    }
+
+    fn op_tsx(&mut self, operand: u8) {
+    
+    }
+
+    fn op_txa(&mut self, operand: u8) {
+    
+    }
+
+    fn op_txs(&mut self, operand: u8) {
+    
     }
 }
